5483067c1dc3e4b5d5ee67ce97d44e104e2bb930
Derek Shockey <derek@alwaysallthetime.com>
2013-08-29 09:56:23 -0700

    add initial set of endpoints for user, post, and token

--- ADNLibModule/src/main/java/com/alwaysallthetime/adnlib/AppDotNetClient.java
+++ ADNLibModule/src/main/java/com/alwaysallthetime/adnlib/AppDotNetClient.java
@@ -3,10 +3,19 @@ package com.alwaysallthetime.adnlib;
 import android.os.AsyncTask;
 import android.os.Build;
 
+import com.alwaysallthetime.adnlib.data.Post;
+import com.alwaysallthetime.adnlib.data.User;
+import com.alwaysallthetime.adnlib.request.AppDotNetApiImageUploadRequest;
+import com.alwaysallthetime.adnlib.request.AppDotNetApiJsonRequest;
+import com.alwaysallthetime.adnlib.request.AppDotNetApiRequest;
 import com.alwaysallthetime.adnlib.request.AppDotNetOAuthRequest;
 import com.alwaysallthetime.adnlib.request.AppDotNetRequest;
 import com.alwaysallthetime.adnlib.response.AccessTokenResponseHandler;
 import com.alwaysallthetime.adnlib.response.LoginResponseHandler;
+import com.alwaysallthetime.adnlib.response.PostResponseHandler;
+import com.alwaysallthetime.adnlib.response.TokenResponseHandler;
+import com.alwaysallthetime.adnlib.response.UserListResponseHandler;
+import com.alwaysallthetime.adnlib.response.UserResponseHandler;
 
 import org.apache.http.NameValuePair;
 import org.apache.http.message.BasicNameValuePair;
@@ -19,6 +28,10 @@ public class AppDotNetClient {
     public static final String METHOD_GET = "GET";
     public static final String METHOD_POST = "POST";
     public static final String METHOD_PUT = "PUT";
+
+    protected static final int ID_LENGTH = 10; // max string length of user ID including delimiter
+    protected static final String ENDPOINT_USERS = "users";
+    protected static final String ENDPOINT_POSTS = "posts";
 
     protected String authHeader;
     protected List<NameValuePair> authParams;
@@ -68,6 +81,231 @@ public class AppDotNetClient {
 
         return params;
     }
+
+    /*
+     * USER
+     */
+
+    public void retrieveUser(String userId, QueryParameters queryParameters, UserResponseHandler responseHandler) {
+        execute(new AppDotNetApiRequest(responseHandler, queryParameters, ENDPOINT_USERS, userId));
+    }
+
+    public void retrieveUser(String userId, UserResponseHandler responseHandler) {
+        retrieveUser(userId, null, responseHandler);
+    }
+
+    public void retrieveCurrentUser(QueryParameters queryParameters, UserResponseHandler responseHandler) {
+        retrieveUser("me", queryParameters, responseHandler);
+    }
+
+    public void retrieveCurrentUser(UserResponseHandler responseHandler) {
+        retrieveCurrentUser(null, responseHandler);
+    }
+
+    public void updateCurrentUser(User user, QueryParameters queryParameters, UserResponseHandler responseHandler) {
+        execute(new AppDotNetApiJsonRequest(responseHandler, METHOD_PUT, user, queryParameters, ENDPOINT_USERS, "me"));
+    }
+
+    public void updateCurrentUser(User user, UserResponseHandler responseHandler) {
+        updateCurrentUser(user, null, responseHandler);
+    }
+
+    public void updateAvatar(byte[] image, int offset, int count, QueryParameters queryParameters, UserResponseHandler responseHandler) {
+        execute(new AppDotNetApiImageUploadRequest(responseHandler, "avatar", image, offset, count, queryParameters,
+                ENDPOINT_USERS, "me/avatar"));
+    }
+
+    public void updateAvatar(byte[] image, int offset, int count, UserResponseHandler responseHandler) {
+        updateAvatar(image, offset, count, null, responseHandler);
+    }
+
+    public void updateAvatar(byte[] image, QueryParameters queryParameters, UserResponseHandler responseHandler) {
+        updateAvatar(image, 0, image.length, queryParameters, responseHandler);
+    }
+
+    public void updateAvatar(byte[] image, UserResponseHandler responseHandler) {
+        updateAvatar(image, null, responseHandler);
+    }
+
+    public void updateCover(byte[] image, int offset, int count, QueryParameters queryParameters, UserResponseHandler responseHandler) {
+        execute(new AppDotNetApiImageUploadRequest(responseHandler, "cover", image, offset, count, queryParameters,
+                ENDPOINT_USERS, "me/cover"));
+    }
+
+    public void updateCover(byte[] image, int offset, int count, UserResponseHandler responseHandler) {
+        updateCover(image, offset, count, null, responseHandler);
+    }
+
+    public void updateCover(byte[] image, QueryParameters queryParameters, UserResponseHandler responseHandler) {
+        updateCover(image, 0, image.length, queryParameters, responseHandler);
+    }
+
+    public void updateCover(byte[] image, UserResponseHandler responseHandler) {
+        updateCover(image, null, responseHandler);
+    }
+
+    public void followUser(String userId, QueryParameters queryParameters, UserResponseHandler responseHandler) {
+        execute(new AppDotNetApiRequest(responseHandler, METHOD_POST, queryParameters, ENDPOINT_USERS, userId, "follow"));
+    }
+
+    public void followUser(String userId, UserResponseHandler responseHandler) {
+        followUser(userId, null, responseHandler);
+    }
+
+    public void unfollowUser(String userId, QueryParameters queryParameters, UserResponseHandler responseHandler) {
+        execute(new AppDotNetApiRequest(responseHandler, METHOD_DELETE, queryParameters, ENDPOINT_USERS, userId, "follow"));
+    }
+
+    public void unfollowUser(String userId, UserResponseHandler responseHandler) {
+        unfollowUser(userId, null, responseHandler);
+    }
+
+    public void muteUser(String userId, QueryParameters queryParameters, UserResponseHandler responseHandler) {
+        execute(new AppDotNetApiRequest(responseHandler, METHOD_POST, queryParameters, ENDPOINT_USERS, userId, "mute"));
+    }
+
+    public void muteUser(String userId, UserResponseHandler responseHandler) {
+        muteUser(userId, null, responseHandler);
+    }
+
+    public void unmuteUser(String userId, QueryParameters queryParameters, UserResponseHandler responseHandler) {
+        execute(new AppDotNetApiRequest(responseHandler, METHOD_DELETE, queryParameters, ENDPOINT_USERS, userId, "mute"));
+    }
+
+    public void unmuteUser(String userId, UserResponseHandler responseHandler) {
+        unmuteUser(userId, null, responseHandler);
+    }
+
+    public void blockUser(String userId, QueryParameters queryParameters, UserResponseHandler responseHandler) {
+        execute(new AppDotNetApiRequest(responseHandler, METHOD_POST, queryParameters, ENDPOINT_USERS, userId, "block"));
+    }
+
+    public void blockUser(String userId, UserResponseHandler responseHandler) {
+        blockUser(userId, null, responseHandler);
+    }
+
+    public void unblockUser(String userId, QueryParameters queryParameters, UserResponseHandler responseHandler) {
+        execute(new AppDotNetApiRequest(responseHandler, METHOD_DELETE, queryParameters, ENDPOINT_USERS, userId, "block"));
+    }
+
+    public void unblockUser(String userId, UserResponseHandler responseHandler) {
+        unblockUser(userId, null, responseHandler);
+    }
+
+    protected void retrieveUsers(String userIds, QueryParameters queryParameters, UserListResponseHandler responseHandler) {
+        if (queryParameters == null)
+            queryParameters = new QueryParameters();
+
+        queryParameters.put("ids", userIds);
+        execute(new AppDotNetApiRequest(responseHandler, queryParameters, ENDPOINT_USERS));
+    }
+
+    public void retrieveUsersById(List<String> userIds, QueryParameters queryParameters, UserListResponseHandler responseHandler) {
+        final StringBuilder buffer = new StringBuilder(userIds.size() * ID_LENGTH);
+        for (final String userId : userIds) {
+            buffer.append(userId);
+            buffer.append(',');
+        }
+        final String userIdsString = buffer.substring(0, buffer.length() - 1);
+        retrieveUsers(userIdsString, queryParameters, responseHandler);
+    }
+
+    public void retrieveUsersById(List<String> userIds, UserListResponseHandler responseHandler) {
+        retrieveUsersById(userIds, null, responseHandler);
+    }
+
+    public void retrieveUsers(List<User> users, QueryParameters queryParameters, UserListResponseHandler responseHandler) {
+        final ArrayList<String> userIds = new ArrayList<String>(users.size());
+        for (final User user : users) {
+            userIds.add(user.getId());
+        }
+        retrieveUsersById(userIds, queryParameters, responseHandler);
+    }
+
+    public void retrieveUsers(List<User> users, UserListResponseHandler responseHandler) {
+        retrieveUsers(users, null, responseHandler);
+    }
+
+    public void retrieveUsersWithSearchQuery(String query, QueryParameters queryParameters, UserListResponseHandler responseHandler) {
+        if (queryParameters == null)
+            queryParameters = new QueryParameters();
+
+        queryParameters.put("q", query);
+        execute(new AppDotNetApiRequest(responseHandler, queryParameters, ENDPOINT_USERS, "search"));
+    }
+
+    public void retrieveUsersWithSearchQuery(String query, UserListResponseHandler responseHandler) {
+        retrieveUsersWithSearchQuery(query, null, responseHandler);
+    }
+
+    /*
+     * POST
+     */
+
+    public void createPost(Post post, QueryParameters queryParameters, PostResponseHandler responseHandler) {
+        execute(new AppDotNetApiJsonRequest(responseHandler, post, queryParameters, ENDPOINT_POSTS));
+    }
+
+    public void createPost(Post post, PostResponseHandler responseHandler) {
+        createPost(post, null, responseHandler);
+    }
+
+    public void retrievePost(String postId, QueryParameters queryParameters, PostResponseHandler responseHandler) {
+        execute(new AppDotNetApiRequest(responseHandler, queryParameters, ENDPOINT_POSTS, postId));
+    }
+
+    public void retrievePost(String postId, PostResponseHandler responseHandler) {
+        retrievePost(postId, null, responseHandler);
+    }
+
+    public void deletePost(String postId, QueryParameters queryParameters, PostResponseHandler responseHandler) {
+        execute(new AppDotNetApiRequest(responseHandler, METHOD_DELETE, queryParameters, ENDPOINT_POSTS, postId));
+    }
+
+    public void deletePost(String postId, PostResponseHandler responseHandler) {
+        deletePost(postId, null, responseHandler);
+    }
+
+    public void repostPost(String postId, QueryParameters queryParameters, PostResponseHandler responseHandler) {
+        execute(new AppDotNetApiRequest(responseHandler, METHOD_POST, queryParameters, ENDPOINT_POSTS, postId, "repost"));
+    }
+
+    public void repostPost(String postId, PostResponseHandler responseHandler) {
+        repostPost(postId, null, responseHandler);
+    }
+
+    public void unrepostPost(String postId, QueryParameters queryParameters, PostResponseHandler responseHandler) {
+        execute(new AppDotNetApiRequest(responseHandler, METHOD_DELETE, queryParameters, ENDPOINT_POSTS, postId, "repost"));
+    }
+
+    public void unrepostPost(String postId, PostResponseHandler responseHandler) {
+        unrepostPost(postId, null, responseHandler);
+    }
+
+    public void starPost(String postId, QueryParameters queryParameters, PostResponseHandler responseHandler) {
+        execute(new AppDotNetApiRequest(responseHandler, METHOD_POST, queryParameters, ENDPOINT_POSTS, postId, "star"));
+    }
+
+    public void starPost(String postId, PostResponseHandler responseHandler) {
+        starPost(postId, null, responseHandler);
+    }
+
+    public void unstarPost(String postId, QueryParameters queryParameters, PostResponseHandler responseHandler) {
+        execute(new AppDotNetApiRequest(responseHandler, METHOD_DELETE, queryParameters, ENDPOINT_POSTS, postId, "star"));
+    }
+
+    public void unstarPost(String postId, PostResponseHandler responseHandler) {
+        unstarPost(postId, null, responseHandler);
+    }
+
+    /*
+     * TOKEN
+     */
+
+    public void retrieveCurrentToken(TokenResponseHandler responseHandler) {
+        execute(new AppDotNetApiRequest(responseHandler, null, "token"));
+    }
+
 
     protected void execute(AppDotNetRequest request) {
         if (request.isAuthenticated() && !hasToken()) {
