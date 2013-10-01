f4ab8612c27a87fd99c72a7c3115b7731647dbad
Derek Shockey <derek@alwaysallthetime.com>
2013-10-01 09:40:24 -0700

    add accessors for pagination metadata

--- ADNLibModule/src/main/java/com/alwaysallthetime/adnlib/response/AppDotNetResponseEnvelopeHandler.java
+++ ADNLibModule/src/main/java/com/alwaysallthetime/adnlib/response/AppDotNetResponseEnvelopeHandler.java
@@ -3,12 +3,15 @@ package com.alwaysallthetime.adnlib.response;
 import com.alwaysallthetime.adnlib.AppDotNetResponseException;
 import com.alwaysallthetime.adnlib.data.IAppDotNetObject;
 import com.alwaysallthetime.adnlib.data.ResponseEnvelope;
+import com.alwaysallthetime.adnlib.data.ResponseMeta;
 import com.google.gson.reflect.TypeToken;
 
 import java.io.Reader;
 import java.net.HttpURLConnection;
 
 public abstract class AppDotNetResponseEnvelopeHandler<T extends IAppDotNetObject> extends AppDotNetResponseHandler<T> {
+    protected ResponseMeta responseMeta;
+
     protected AppDotNetResponseEnvelopeHandler(TypeToken<ResponseEnvelope<T>> typeToken) {
         super(typeToken);
     }
@@ -16,11 +19,37 @@ public abstract class AppDotNetResponseEnvelopeHandler<T extends IAppDotNetObjec
     @Override
     public void handleResponse(Reader reader) {
         final ResponseEnvelope<T> response = parseResponse(reader);
-        final int status = response.getMeta().getCode();
-        if (status != HttpURLConnection.HTTP_OK) {
-            onError(new AppDotNetResponseException(response.getMeta()));
+        responseMeta = response.getMeta();
+
+        if (responseMeta.getCode() != HttpURLConnection.HTTP_OK) {
+            onError(new AppDotNetResponseException(responseMeta));
         } else {
             onSuccess(response.getData());
         }
+    }
+
+    public ResponseMeta getResponseMeta() {
+        return responseMeta;
+    }
+
+    public String getMaxId() {
+        if (responseMeta == null)
+            return null;
+
+        return responseMeta.getMaxId();
+    }
+
+    public String getMinId() {
+        if (responseMeta == null)
+            return null;
+
+        return responseMeta.getMinId();
+    }
+
+    public boolean isMore() {
+        if (responseMeta == null)
+            return false;
+
+        return responseMeta.isMore();
     }
 }
