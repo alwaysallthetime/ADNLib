c8aeacf1385afdc8bd8532041b179a398a7f5108
Rob Brambley <rrbrambley@gmail.com>
2013-10-05 22:42:58 -0700

    add file and associated data structures

---
+++ ADNLibModule/src/main/java/com/alwaysallthetime/adnlib/data/ImageInfo.java
@@ -0,0 +1,14 @@
+package com.alwaysallthetime.adnlib.data;
+
+public class ImageInfo implements IAppDotNetObject {
+    private int width;
+    private int height;
+
+    public int getWidth() {
+        return width;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+}
