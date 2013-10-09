56a89bfbe673e3e5b002d2daa44a6b299d36851e
Rob Brambley <rrbrambley@gmail.com>
2013-10-09 11:41:45 -0700

    add a class for annotation constants
    WIP

---
+++ ADNLibModule/src/main/java/com/alwaysallthetime/adnlib/Annotations.java
@@ -0,0 +1,24 @@
+package com.alwaysallthetime.adnlib;
+
+public class Annotations {
+
+    /*
+     * CORE
+     */
+    public static final String OEMBED = "net.app.core.oembed";
+    public static final String GEOLOCATION = "net.app.core.geolocation";
+    public static final String CHECKIN = "net.app.core.checkin";
+    public static final String CROSSPOST = "net.app.core.crosspost";
+
+    /*
+     * REPLACEMENT
+     */
+    public static final String REPLACEMENT_PLACE = "+net.app.core.place";
+    public static final String REPLACEMENT_FILE = "+net.app.core.file";
+
+
+    /*
+     * THIRD PARTY
+     */
+    public static final String OHAI_DATE = "net.app.ohai.displaydate";
+}
