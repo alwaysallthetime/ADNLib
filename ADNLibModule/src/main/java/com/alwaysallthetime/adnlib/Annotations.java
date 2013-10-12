a46f82843f70d13f76515f6f67644a98aac0debe
Rob Brambley <rrbrambley@gmail.com>
2013-10-12 09:59:55 -0700

    add all the core annotations

--- ADNLibModule/src/main/java/com/alwaysallthetime/adnlib/Annotations.java
+++ ADNLibModule/src/main/java/com/alwaysallthetime/adnlib/Annotations.java
@@ -8,17 +8,28 @@ public class Annotations {
     /*
      * CORE
      */
-    public static final String OEMBED = "net.app.core.oembed";
-    public static final String GEOLOCATION = "net.app.core.geolocation";
+    public static final String ATTACHMENTS = "net.app.core.attachments";
+    public static final String CHANNEL_INVITE = "net.app.core.channel.invite";
     public static final String CHECKIN = "net.app.core.checkin";
     public static final String CROSSPOST = "net.app.core.crosspost";
+    public static final String DIRECTORY_BLOG = "net.app.core.directory.blog";
+    public static final String DIRECTORY_FACEBOOK = "net.app.core.directory.facebook";
+    public static final String DIRECTORY_HOMEPAGE = "net.app.core.directory.homepage";
+    public static final String DIRECTORY_TWITTER = "net.app.core.directory.twitter";
+    public static final String FALLBACK_URL = "net.app.core.fallback_url";
+    public static final String GEOLOCATION = "net.app.core.geolocation";
+    public static final String LANGUAGE = "net.app.core.language";
+    public static final String OEMBED = "net.app.core.oembed";
+    public static final String OEMBED_METADATA = "net.app.core.oembed.metadata";
+    public static final String PHOTOTAG = "net.app.core.phototag";
 
     /*
      * REPLACEMENT
      */
-    public static final String REPLACEMENT_PLACE = "+net.app.core.place";
     public static final String REPLACEMENT_FILE = "+net.app.core.file";
-
+    public static final String REPLACEMENT_FILE_LIST = "+net.app.core.file_list";
+    public static final String REPLACEMENT_PLACE = "+net.app.core.place";
+    public static final String REPLACEMENT_USER = "+net.app.core.user";
 
     /*
      * THIRD PARTY
