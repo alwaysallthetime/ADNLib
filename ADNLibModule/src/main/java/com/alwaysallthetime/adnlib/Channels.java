0691e39eff67b4447fc22eaf953fb71886a5d0e6
Rob Brambley <rrbrambley@gmail.com>
2013-10-21 09:14:06 -0700

    add Channels for channel type constants

---
+++ ADNLibModule/src/main/java/com/alwaysallthetime/adnlib/Channels.java
@@ -0,0 +1,7 @@
+package com.alwaysallthetime.adnlib;
+
+public class Channels {
+    public static final String PM = "net.app.core.pm";
+    public static final String OHAI_JOURNAL = "net.app.ohai.journal";
+    public static final String PATTER_ROOM = "net.patter-app.room";
+}
