9dce88175461bd8df2ac48f928814489a5171b74
Bryan Berg <bryan@mixedmedialabs.com>
2013-11-20 17:27:44 -0800

    getUrlForSize() method, like ADNKit's

--- ADNLibModule/src/main/java/com/alwaysallthetime/adnlib/data/Image.java
+++ ADNLibModule/src/main/java/com/alwaysallthetime/adnlib/data/Image.java
@@ -1,5 +1,7 @@
 package com.alwaysallthetime.adnlib.data;
 
+import android.net.Uri;
+
 import com.google.gson.annotations.Expose;
 
 public class Image {
@@ -19,6 +21,21 @@ public class Image {
 
     public String getUrl() {
         return url;
+    }
+
+    public String getUrlForSize(int width, int height) {
+        Uri uri = Uri.parse(getUrl());
+        Uri.Builder builder = uri.buildUpon();
+
+        if (width > 0) {
+            builder.appendQueryParameter("w", String.valueOf(width));
+        }
+
+        if (height > 0) {
+            builder.appendQueryParameter("h", String.valueOf(height));
+        }
+
+        return builder.build().toString();
     }
 
     public boolean isDefault() {
