591ec4beba5a633324b67107912ab81a9c0c0c03
Rob Brambley <rrbrambley@gmail.com>
2014-01-03 12:36:04 -0800

    add method for combining two sets of QueryParameters

--- ADNLibModule/src/main/java/com/alwaysallthetime/adnlib/QueryParameters.java
+++ ADNLibModule/src/main/java/com/alwaysallthetime/adnlib/QueryParameters.java
@@ -8,6 +8,23 @@ public class QueryParameters extends HashMap<String, String> {
     private static final int INITIAL_CAPACITY = 4;
     private static final float LOAD_FACTOR = 1.0f;
 
+    /**
+     * Get a new QueryParameters that consists of two sets of QueryParamters combined.
+     *
+     * Since it is possible that there will be conflicting parameters, the second set will be applied
+     * "on top of" the first set, i.e. the topParameters can override the bottomParameters
+     *
+     * @param bottomParameters
+     * @param topParameters
+     * @return a new QueryParamters consisting of the values form both specified QueryParameters objects.
+     */
+    public static QueryParameters getCombinedParameters(QueryParameters bottomParameters, QueryParameters topParameters) {
+        QueryParameters newParams = new QueryParameters();
+        newParams.putAll(bottomParameters);
+        newParams.putAll(topParameters);
+        return newParams;
+    }
+
     public QueryParameters() {
         this(INITIAL_CAPACITY, LOAD_FACTOR);
     }
